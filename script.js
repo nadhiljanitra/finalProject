acb65c7d05f190bc4ffc3c91fe64aa8f04be7b05
nadhiljanitra <nadhiljanitraa@gmail.com>
2019-08-30 14:57:12 +0700

    add star

--- script.js
+++ script.js
@@ -4,35 +4,79 @@ title.innerHTML = "Uncle Joe Feedback Site";
 head.appendChild(title);
 
 var list = {
-  "food" : ["Taste","Variety of Menu","Platting","Price"],
-  "restaurant" : ["Sanitation","Ambience","Facility","Access Location"],
-  "server" : ["Attentive and Quick","Knowledgable and Quick","knowledgeable and courteous","Attitude and Friendly"]
-}
+  food: ["Taste", "Variety of Menu", "Platting", "Price", "hygine"],
+  restaurant: ["Sanitation", "Ambience", "Facility", "Access Location"],
+  server: [
+    "Attentive and Quick",
+    "Knowledgable and Quick",
+    "knowledgeable and courteous",
+    "Attitude and Friendly"
+  ]
+};
 
-for (key in list){
-  if (key == "food"){
-    var f = document.querySelector("#food")
-    for (var i = 0; i<list[key].length;i++){
-      var f1 = document.createElement("div")
-      f1.innerHTML = list[key][i] + ' :'
-      f.appendChild(f1)
+for (key in list) {
+  if (key == "food") {
+    var f = document.querySelector("#food");
+    for (var i = 0; i < list[key].length; i++) {
+      var f1 = document.createElement("div");
+      f1.innerHTML = list[key][i] + " :";
+      f.appendChild(f1);
     }
-  } else if (key == "restaurant"){
-    var f = document.querySelector("#restaurant")
-    for (var i = 0; i<list[key].length;i++){
-      var f1 = document.createElement("div")
-      f1.innerHTML = list[key][i] + ' :'
-      f.appendChild(f1)
+  } else if (key == "restaurant") {
+    var f = document.querySelector("#restaurant");
+    for (var i = 0; i < list[key].length; i++) {
+      var f1 = document.createElement("div");
+      f1.innerHTML = list[key][i] + " :";
+      f.appendChild(f1);
     }
-  } else if (key == "server"){
-    var f = document.querySelector("#server")
-    for (var i = 0; i<list[key].length;i++){
-      var f1 = document.createElement("div")
-      f1.innerHTML = list[key][i] + ' :'
-      f.appendChild(f1)
+  } else if (key == "server") {
+    var f = document.querySelector("#server");
+    for (var i = 0; i < list[key].length; i++) {
+      var f1 = document.createElement("div");
+      f1.innerHTML = list[key][i] + " :";
+      f.appendChild(f1);
     }
   }
 }
+var rate = document.getElementsByClassName("rating");
+console.log(rate);
+for (i = 0; i < rate.length; i++) {
+  var sibling = rate[i].previousElementSibling;
+  console.log(sibling.children);
+  for (j = 0; j < sibling.children.length; j++) {
+    var eachRate = document.createElement("div");
+    eachRate.setAttribute("id", "rate");
+    rate[i].appendChild(eachRate);
+  }
+}
+var sRate = document.querySelectorAll("#rate");
+var test = Array.from(sRate);
+console.log(sRate);
+console.log(test);
+for (i = 0; i < test.length; i++) {
+  for (j = 1; j < 6; j++) {
+    var star = document.createElement("span");
+    star.setAttribute("value", `${j}`);
+    test[i].appendChild(star);
+  }
+  test[i].addEventListener("click", function(e) {
+    let action = "add";
+    for (const span of this.children) {
+      span.classList[action]("active");
+      if (span === e.target) {
+        action = "remove";
+      }
+      var val = document.querySelector("span").getAttribute("value");
+      console.log(val);
+    }
+    console.log(e.target.getAttribute("value"));
+  });
+}
+
+
+// var bintangSatuan = createElement("span")
+// before = rate[0].previousElementSibling;
+// console.log(before)
 
 var coll = document.querySelectorAll(".category");
 for (var i = 0; i < coll.length; i++) {
